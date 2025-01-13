8dff45917b5c7a9d14bf71027aa389302048c469
JekSkeez <98408944+JekSkeez@users.noreply.github.com>
2025-01-14 02:31:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,6 @@ async def stop(update: Update, context: CallbackContext):
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
